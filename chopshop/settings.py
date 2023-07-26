89c90a3b729b47cf2d4de730cdc0152ec7be445c
DEVANSHU KHEMKA <126005633+devanshu-25-04@users.noreply.github.com>
2023-07-26 22:45:38 +0530

    Add files via upload

--- chopshop/settings.py
+++ chopshop/settings.py
@@ -141,5 +141,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_HOST_USER = 'redrondoofdeath@gmail.com'
-EMAIL_HOST_PASSWORD = 'bapeockmyuxkvnnq'
+EMAIL_HOST_USER = 'traskgaming765@gmail.com'
+EMAIL_HOST_PASSWORD = 'wtrpodlctykjhqyj'
